99a76f5f38ada858407b55d69baae72ef19bd9ce
NAMIKI IWAKI <namiki@NAMIKInoMacBook-Air.local>
2025-11-06 19:31:07 +0900

    test

---
+++ example.py
@@ -0,0 +1,19 @@
+print("hello world")
+
+def greet():
+    print("こんにちは")
+greet()
+
+def Print_name(name):
+    print(f"私の名前は{name}です")
+Print_name("ナッキー")
+
+def get_greet():
+    return "おはようございます"
+print(get_greet())
+
+def add(a,b):
+    return a + b
+result = add(3,7)
+print(result)
+
